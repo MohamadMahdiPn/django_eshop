fd4a4265a5566b0433225b2176865f1d208b4cdf
محمد مهدی پورنجفی <m.p_996@hotmail.com>
2023-12-27 19:24:04 +0330

    Add Post and Get

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import contactUser
 
 
 class ContactForm(forms.Form):
@@ -7,11 +8,33 @@ class ContactForm(forms.Form):
     },
                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "نام"}),
                                label='نام و نام خانوادگی')
-    email = forms.EmailField(required=True, label='ایمیل', widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
-    subject = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "نام"}), label='موضوع')
+    email = forms.EmailField(required=True, label='ایمیل',
+                             widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    subject = forms.CharField(max_length=200,
+                              widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "نام"}),
+                              label='موضوع')
     text = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control',
-                                                        'placeholder': 'توضیحات' ,
+                                                        'placeholder': 'توضیحات',
                                                         'cols': '40',
                                                         'rows': '5',
                                                         'id': 'message'
                                                         }), label='توضیحات')
+
+
+class ContactUserModelForm(forms.ModelForm):
+    class Meta:
+        model = contactUser
+        fields = ['fullName', 'email', 'message', 'title']
+        # fields = '__all__'
+        # exclude = ['createdDate']
+        widgets = {
+            'fullName': forms.TextInput(attrs={'class': 'form-control', 'placeholder': "نام"}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+            'message': forms.Textarea(attrs={'class': 'form-control',
+                                             'placeholder': 'توضیحات',
+                                             'cols': '40',
+                                             'rows': '5',
+                                             'id': 'message'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': "موضوع"})
+
+        }
--- contact_module/views.py
+++ contact_module/views.py
@@ -1,18 +1,42 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.shortcuts import reverse
+from django.views import View
 
-from contact_module.forms import ContactForm
+from contact_module.forms import ContactForm, ContactUserModelForm
+from contact_module.models import contactUser
+
+
+class ContactView(View):
+    def get(self, request):
+        contact_form = ContactUserModelForm()
+        return render(request, 'contact_module/contact_us_page.html', {'contact_form': contact_form})
+
+    def post(self, request):
+        form = ContactUserModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
 
 
 # Create your views here.
 def contact_us_page(request):
     # contact_form = ContactForm(request.POST or None)
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        # form = ContactForm(request.POST)
+        form = ContactUserModelForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            # print(form.cleaned_data)
+            # contactDb = contactUser(form.cleaned_data)
+            # fullName=form.cleaned_data['fullName'],
+            # email=form.cleaned_data['email'],
+            # message=form.cleaned_data['text'],
+            # isRead=False,
+            # title=form.cleaned_data['subject']
+            # )
+            form.save()
+
             return HttpResponseRedirect(reverse('index'))
     else:
-        contact_form = ContactForm()
+        contact_form = ContactUserModelForm()
     return render(request, 'contact_module/contact_us_page.html', {'contact_form': contact_form})
