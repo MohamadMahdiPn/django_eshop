233c712bad8720eaf2a511ca037671551061b406
mamad <m.p_996@hotmail.com>
2024-02-02 12:21:56 +0330

    add category view

--- home_module/views.py
+++ home_module/views.py
@@ -1,9 +1,8 @@
 from django.db.models import Count
 from django.shortcuts import render
-from django.views import View
 from django.views.generic.base import TemplateView
 from Utils.Convertors import group_list
-from product.models import Product
+from product.models import Product, ProductCategory
 from site_module.models import SiteSetting, FooterLinkBox, Slider
 
 
@@ -16,11 +15,23 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['data'] = 'this is data From home page'
         context['Sliders'] = Slider.objects.filter(is_Active=True)
+
         latest_products = Product.objects.filter(isActive=True).order_by('-id')[:12]
         context['latest_products'] = group_list(latest_products, 2)
-        most_visited_products = Product.objects.filter(isActive=True,).annotate(visit_count=Count('productvisit')).order_by('productvisit')[:12]
 
+        most_visited_products = Product.objects.filter(isActive=True,).annotate(visit_count=Count('productvisit')).order_by('productvisit')[:12]
         context['most_visited_products'] = group_list(most_visited_products, 2)
+
+        categories = list(ProductCategory.objects.annotate(products_count=Count('Categoties')).filter(is_Active=True, is_delete=False, products_count__gt=0)[:6])
+        categories_products = []
+        for category in categories:
+            item = {
+                'id': category.id,
+                'title': category.title,
+                'products': list(category.Categoties.all()[:4])
+            }
+            categories_products.append(item)
+        context['categories_Products'] = categories_products
         return context
     # def get(self, request):
     #     return render(request, 'home_module/index_page.html')
--- polls/templatetags/poll_extras.py
+++ polls/templatetags/poll_extras.py
@@ -3,6 +3,10 @@ from jalali_date import date2jalali
 register = template.Library()
 
 
+@register.filter(name='threeDigitsCurrency')
+def threeDigitsCurrency(value: int):
+    return '{:,}'.format(value)
+
 @register.filter(name='cut')
 def cut(value, arg):
     """Removes all values of arg from the given string"""
@@ -14,4 +18,5 @@ def showJalaliDate(value):
     return date2jalali(value)
 
 # register.filter("cut", cut)
+
 
